1df2c1e696196f1286efe25ce9565ffec513df1d
okamoto haruka <100815156+okamoto-haruka@users.noreply.github.com>
2022-06-14 04:10:57 +0000

    [update] たぶんこれでsubmoduleの更新ができるはず

--- etc/make_wago_json.py
+++ etc/make_wago_json.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import json
 
-PATH = 'wago.121808.pn'
+PATH = "wago.121808.pn"
 with open(PATH) as fd:
     wago_dict = {}
     for line in fd:
         try:
-            polarity, word = line.rstrip().split('\t')
-            word = word.replace(' だ', '').replace(' と', '').replace(' の', '').replace(' です', '')
-            word = word.replace(' ある', '')
-        except:
+            polarity, word = line.rstrip().split("\t")
+            word = (
+                word.replace(" だ", "")
+                .replace(" と", "")
+                .replace(" の", "")
+                .replace(" です", "")
+            )
+            if word.endswith(" ある") or word.endswith(" ない"):
+                continue
+        except Exception:
             continue
         wago_dict[word] = polarity
-json.dump(wago_dict, open('pn_wago.json', 'w'))
+json.dump(wago_dict, open("pn_wago.json", "w"))
